196ec5afe43a588ce6aaac15c47f531f47ea96cd
Sharan Kudtarkar <SK19@ador.co.in>
2024-02-21 10:07:06 +0530

    02/21/24 10:06:57

--- src/Components/Path.jsx
+++ src/Components/Path.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, Navigate, useLocation } from "react-router-dom";
 import LoginPage from './LoginPage/LoginPage'
 import ErrorPage404NotFound from './ErrorPage404NotFound';
 import HomePage from './HomePage/HomePage';
 import SharePage from './ClientShare/SharePage';
 import { getCookies } from '../HelperComponents/CookiesS';
+
 function Path() {
+    if (window.location.pathname.includes("/share")) {
+        return (
+            <Router>
+                <Routes>
+                    <Route path='/share/:id' element={<SharePage />} />
+                </Routes>
+            </Router>
+        )
+    }
 
     if (!getCookies()[0]) {
         return <>
@@ -17,7 +27,7 @@ function Path() {
                 </Routes>
             </Router>
         </>
-    } else if (getCookies()[0]) {
+    } else {
         return (
             <Router>
                 <Routes>
@@ -28,13 +38,6 @@ function Path() {
                 </Routes>
             </Router>
         )
-    }
-    else {
-        <Router>
-            <Routes>
-                <Route path='/share/:id' element={<SharePage />} />
-            </Routes>
-        </Router>
     }
 
 }
